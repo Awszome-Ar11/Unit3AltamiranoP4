42b36704facc3e084cf0092f56e792de4be2a4a7
Awszome-Ar11 <743241@student.dusd.net>
2026-04-03 20:19:52 -0700

    added the extra stuff
    double jump

--- P4AltamiranoUnit3/Assets/Scripts/MoveLeft.cs
+++ P4AltamiranoUnit3/Assets/Scripts/MoveLeft.cs
@@ -17,6 +17,10 @@ public class MoveLeft : MonoBehaviour
     {
         if (playerControllerScript.gameOver == false)
         {
+            transform.Translate(Vector3.left * Time.deltaTime * (speed * 2));
+        }
+        else
+        {
           transform.Translate(Vector3.left * Time.deltaTime * speed);
 
         }
--- P4AltamiranoUnit3/Assets/Scripts/PlayerController.cs
+++ P4AltamiranoUnit3/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,9 @@ public class PlayerController : MonoBehaviour
     public AudioClip crashSound;
     private AudioSource playerAudio;
 
+    public bool doubleJumpUsed = false;
+    public float doubleJumpForce;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -33,6 +36,27 @@ public class PlayerController : MonoBehaviour
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+
+            doubleJumpUsed = false;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+            playerAnim.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+
+        
+
+
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+            playerAnim.SetTrigger("Jump_trig");
             playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
